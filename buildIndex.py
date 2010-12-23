00bb7648c57c4920f488e9ea7d00f61ef88ff7bd
Gary Bishop <gary.bishop.unc@gmail.com>
2010-12-23 08:21:34 -0500

    Index the rendered html instead of the source files.

--- buildIndex.py
+++ buildIndex.py
@@ -1,5 +1,7 @@
 #!/home/gb/bin/python2.6
 
+ignoreDirs = [ 'css', 'fonts', 'indexdir', 'archive', 'category', 'feed', 'page' ]
+
 import os, os.path
 from whoosh import index
 from whoosh.fields import *
@@ -15,23 +17,31 @@ schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
 ix = index.create_in(indexDir, schema)
 writer = ix.writer()
 
-postdir = '_posts'
-
-posts = os.listdir(postdir)
-
-permalink = re.compile(r'(?<=^permalink:).*$', re.M)
-title = re.compile(r'(?<=^title:).*$', re.M)
-divider = re.compile(r'^---$', re.M)
-
-for post in posts:
-    text = file(os.path.join(postdir, post), 'r').read()
-    empty, header, content = divider.split(text)
-    P = permalink.search(header).group(0).strip().decode('utf-8')
-    T = title.search(header).group(0).strip().decode('utf-8')
-    C = T + ' ' + ''.join(BeautifulSoup(content.decode('utf-8')).findAll(text=True))
-    writer.add_document(title=T, path=P, content=C)
+count = 0
+for root, dirs, files in os.walk('_site'):
+    for d in ignoreDirs:
+        if d in dirs:
+            dirs.remove(d)
+            
+    for f in files:
+        if not f.endswith('.html'):
+            continue
+        count += 1
+        fpath = os.path.join(root, f)
+        parts = fpath.split('/')
+        parts[0] = '~gb'
+        if parts[-1] == 'index.html':
+            del parts[-1]
+        upath = '/'.join(parts)
+        soup = BeautifulSoup(file(fpath, 'r'))
+        content = soup.find('div', id="main")
+        title = content.find('h1').string
+        permalink = upath.decode('utf-8')
+        text = ' '.join(content.findAll(text=True))
+        writer.add_document(title=title, path=permalink, content=text)
+        
 writer.commit()
 t1 = time.time()
 
-print t1 - t0, len(posts)
+print t1 - t0, count
 
